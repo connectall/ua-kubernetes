9e57ec8cfd3ee4de11cb6af46464deee6b33b238
dougbass <dougbass1@yahoo.com>
2020-12-30 13:37:12 -0800

    Return the build results as an attachment

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,14 @@
 epoch_time=`date +%s`
 echo time = $epoch_time
 if [ $((epoch_time%2)) -eq 0 ]; then
-	log=`cat error.log|sed 's/\"/\\"/g' | perl -p -e 's/\n/\\\\n/'`
+	#log=`cat error.log|sed 's/\"/\\"/g' | perl -p -e 's/\n/\\\\n/'`
+	log='/error.log'
 #  | jq -aRs .`
 	test_status='fail'
 	echo  'test failed'
 else
-	log=`cat success.log|sed 's/\"/\\"/g' | perl -p -e 's/\n/\\\\n/'`
+	log='/success.log'
+	#log=`cat success.log|sed 's/\"/\\"/g' | perl -p -e 's/\n/\\\\n/'`
 #  | jq -aRs .`
 	test_status='pass'
 	echo 'test passed'
@@ -21,6 +23,5 @@ fi
 # Remove the unique part of the job name on the end, eg, make TEST-123-jjdh to TEST-123
 #
 recordid=`echo ${recordid}|sed 's/\(.*\)-.*/\1/'`
-echo "$log" >z.z
 
 ./post_results.sh "${applink}" "${recordid}" "${log}" "${test_status}"
--- post_results.sh
+++ post_results.sh
@@ -10,9 +10,17 @@ json="
         \"test_log\":\"$3\",
         \"test_status\":\"$4\"
         }}"
+echo
 echo "Send JSON: $json"
 echo "via ${ConnectAllUrl}/api/2/postRecord?apikey=$ConnectAllApikey"
-curl \
+curl -s \
 --header "Content-Type: application/json;charset=UTF-8" -X POST \
 -d "$json" \
  ${ConnectAllUrl}/connectall/api/2/postRecord?apikey=$ConnectAllApiKey
+
+
+# Send log file as an attachment
+echo
+echo "send the file $3 as an attachment"
+echo cmd: curl -s --location -X POST --header 'Content-Type: multipart/form-data' --form "file=@${3}" "${ConnectAllUrl}/connectall/api/2/upload?appLinkName=${applink}&origin=destination&recordId=$2&apikey=$ConnectAllApiKey"
+curl -s --location -X POST --header 'Content-Type: multipart/form-data' --form "file=@${3}" "${ConnectAllUrl}/connectall/api/2/upload?appLinkName=${applink}&origin=destination&recordId=$2&apikey=$ConnectAllApiKey"
